8eb26369ca4dbe4c9127e6387a9c20d221b1a1d7
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-08-24 11:33:06 +0700

    feat: intergrate jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,16 +2,25 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { LoginAuthDto } from './dto/login-auth.dto';
 import { UsersService } from 'src/users/users.service';
 import { comparePassword } from 'src/utils/hash';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) { }
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) { }
   async login(loginAuthDto: LoginAuthDto) {
+    // check credential
     const { email, password } = loginAuthDto
     const user = await this.usersService.findByEmail(email)
     const isMatch = await comparePassword(password, user.password)
-    if (isMatch)
-      return user
-    throw new HttpException("Bad credential", HttpStatus.UNAUTHORIZED)
+    if (!isMatch)
+      throw new HttpException("Bad credential", HttpStatus.UNAUTHORIZED)
+    //generate JWT
+    const payload = { sub: user.id, username: user.email };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
