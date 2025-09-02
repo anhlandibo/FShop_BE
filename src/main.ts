967e356c5c6fd70323e10cc1f11572ac1c2682ba
anhlandibo <ngominhtri9107@gmail.com>
2025-09-02 22:57:48 +0700

    config: adding swagger for APIs docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // prefix API
+  app.setGlobalPrefix('api/v1');
+  // swagger 
+  const config = new DocumentBuilder()
+    .setTitle('Fashion Shop API')
+    .setDescription('Build APIs cho fashion shop website')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs', app, documentFactory);
+  const logger = new Logger('bootstrap');
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  logger.log(`Server is running on port ${port}`);
+  logger.log(`Swagger is running on http://localhost:${port}/api/v1/docs`);
 }
 bootstrap();
